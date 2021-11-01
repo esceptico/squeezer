60f20d27252257938a1c123a33f6423f99c9bbde
Timur Ganiev <tim9624@gmail.com>
2021-11-02 01:46:32 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+import pkg_resources
+from setuptools import setup
+
+
+PATH_ROOT = os.path.realpath(os.path.dirname(__file__))
+REQUIREMENTS_PATH = os.path.join(PATH_ROOT, 'requirements.txt')
+
+
+def parse_requirements(path: str):
+    with open(path) as fp:
+        text = fp.read()
+    requirements = [
+        str(requirement) for requirement
+        in pkg_resources.parse_requirements(text)
+    ]
+    return requirements
+
+
+setup(
+    name='squeezer',
+    version='0.0.1',
+    packages=['squeezer'],
+    url='https://github.com/esceptico/squeezer',
+    license='MIT',
+    author='a18282463',
+    author_email='ganiev.tmr@gmail.com',
+    description='Lightweight knowledge distillation pipeline',
+    install_requires=parse_requirements(REQUIREMENTS_PATH)
+)
